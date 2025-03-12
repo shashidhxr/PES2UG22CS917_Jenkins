9f2123bc44af31a39f87afbc37c744b3c5d1c9aa
Shashidhar Biradar <shashidharb459@gmail.com>
2025-03-12 23:19:53 +0530

    modified hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully setup the webhook  \n";
   return 0;
 }
